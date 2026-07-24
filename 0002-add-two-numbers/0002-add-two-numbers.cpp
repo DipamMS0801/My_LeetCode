aa2159ace41a9bdc8af5e5ff3b08936d1cab128e
Dipam M Shendurkar <181740098+DipamMS0801@users.noreply.github.com>
2026-07-24 12:17:37 +0530

    Time: 4 ms (21.18%), Space: 77.1 MB (75.78%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+
+    ListNode* Reverse(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* fut = NULL;
+        ListNode* prev = NULL;
+        while (curr != NULL) {
+            fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
+        }
+        return prev;
+    }
+
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // while (l1 && l1->next && l1->val == 0) {
+        //     ListNode* temp = l1;
+        //     l1 = l1->next;
+        //     delete temp;
+        // }
+
+        // while (l2 && l2->next && l2->val == 0) {
+        //     ListNode* temp = l2;
+        //     l2 = l2->next;
+        //     delete temp;
+        // }
+
+        // l1 = Reverse(l1);
+        // l2 = Reverse(l2);
+
+        ListNode* curr1 = l1;
+        ListNode* curr2 = l2;
+
+        ListNode* head = new ListNode(0);
+        ListNode* tail = head;
+
+        int sum = 0, carry = 0;
+
+        while (curr1 || curr2 || carry > 0) {
+            sum = carry;
+            if (curr1 != NULL) {
+                sum += curr1->val;
+                curr1 = curr1->next;
+            }
+            if (curr2 != NULL) {
+                sum += curr2->val;
+                curr2 = curr2->next;
+            }
+
+            carry = sum / 10;
+            tail->next = new ListNode(sum % 10);
+            tail = tail->next;
+        }
+        ListNode *ans=head->next;
+		delete head;
+        return ans;
+    }
+};
