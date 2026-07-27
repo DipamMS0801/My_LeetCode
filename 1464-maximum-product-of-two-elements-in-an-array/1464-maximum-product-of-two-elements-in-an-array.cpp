c8fc972ab77d45a4016bf9c8ad6941a45bf2dd50
Dipam M Shendurkar <181740098+DipamMS0801@users.noreply.github.com>
2026-07-27 12:51:48 +0530

    Time: 0 ms (100%), Space: 13.4 MB (85.48%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -4,12 +4,9 @@ public:
         sort(nums.begin(), nums.end());
         int ans = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
+        
             ans = ((nums[n - 1] - 1) * (nums[n - 2] - 1));
-        }
+        
         return ans;
-
-       
-
     }
 };
