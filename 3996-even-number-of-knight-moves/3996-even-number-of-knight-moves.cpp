b3c13550caead5daebabd920b9d490b6fe0556c1
Dipam M Shendurkar <181740098+DipamMS0801@users.noreply.github.com>
2026-07-30 16:07:47 +0530

    Time: 0 ms (100%), Space: 49.7 MB (39.96%) - LeetHub

---
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canReach(vector<int>& start, vector<int>& target) {
+        if((start[0]+start[1])%2==(target[0]+target[1])%2){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
